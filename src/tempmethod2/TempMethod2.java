60d8faac274199c48069e5380198de1cffdef5f0
Łukasz <ukasz@HaPik>
2019-04-25 12:10:00 +0200

    This is a TempMethod project with some testing activities.

---
+++ src/tempmethod2/TempMethod2.java
@@ -0,0 +1,62 @@
+
+package tempmethod2;
+
+import java.util.Scanner;
+
+public class TempMethod2 {
+    
+    double Ctemp = 0;
+    double Ftemp = 0;
+    double Ktemp = 0;
+        
+     void getTemperature(){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj temperaturę: ");
+        Ctemp = scan.nextDouble();
+        
+        if (Ctemp > 0)
+        
+        {
+            System.out.println("Twoja temperatura to: " +  Ctemp + "." + " " + "Temperatura jest dodatnia."); 
+        }
+        
+        else if (Ctemp < 0)
+
+        {
+            System.out.println("Twoja temperatura to: " + Ctemp + "." + " " + "Temperatura jest ujemna.");
+        }
+        
+        else 
+        {
+            System.out.println("Twoja temperatura to: " + Ctemp + "." + " " + "Temperatura wynosi 0.");
+        }
+    }
+     
+     void getFahrenheitTemp(){
+         Ftemp = (Ctemp * 1.8) + 32;
+         
+         System.out.println("Temperatura w Fahrenheit wynosi: " + Ftemp);
+         
+     }
+     
+     void getKelvinTemp(){
+         
+         Ktemp = Ctemp - 273.15;
+         
+         System.out.println("Temperatura w Kalvinach wynosi: " + Ktemp);
+         
+     }
+     
+     public double getDecimalTemperature(){
+         
+         return 5/9 * Ftemp - 32;
+     }
+}
+
+
+
+     
+
+
+     
---
+++ src/tempmethod2/TempMethod2Main.java
@@ -0,0 +1,15 @@
+package tempmethod2;
+
+public class TempMethod2Main {
+
+    public static void main(String[] args) {
+        
+        TempMethod2 t = new TempMethod2();
+        
+        t.getTemperature();
+        t.getFahrenheitTemp();
+        t.getKelvinTemp();
+
+    }
+
+}
